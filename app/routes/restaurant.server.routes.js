89bcdf1c0e55d62147ca4ae0569a729a73389fb7
Don <Don@alphamail>
2014-12-30 17:23:11 -0600

    create, update restaurant

--- app/routes/restaurant.server.routes.js
+++ app/routes/restaurant.server.routes.js
@@ -4,20 +4,20 @@
 // Define the routes module' method
 module.exports = function (app) {
     var daoService = app.daoService;
-    
-    
+
+
     //@getRestaurant(id)
     app.get('/restaurant/:id', function (req, res) {
 
         var restaurantId = parseInt(req.params.id);
         console.log("restaurant id " + restaurantId);
         var restaurantFound = daoService.getRestaurantById(restaurantId);
-        console.log("found "+restaurantFound);
-        
-        var resVar ={};
+        console.log("found " + restaurantFound);
+
+        var resVar = {};
         resVar.biteMe = "get a job";
-      
-        
+
+
         if (restaurantFound == null)
         {
             resVar = daoService.createError('Not Found', "NotFoundClass");
@@ -29,23 +29,50 @@ module.exports = function (app) {
             resVar = restaurantFound;
             res.json(resVar);
         }
-        
+
 
     });
-    
+
     //@getAllRestaurants
     app.get('/restaurant', function (req, res) {
-    res.json(daoService.getAllRestaurants());
+        res.json(daoService.getAllRestaurants());
 
-});
-    
-    
-    
-    
-    
-    
-    
-    
+    });
+
+
+    //@create restaurant
+    app.post('/restaurant', function (req, res) {
+        // console.log(req.body);
+        daoService.createRestaurant(req.body);
+        var resVar = daoService.createIdResponse(req.body.id);
+        res.json(resVar);
+    });
+
+
+//@update restaurant
+    app.put('/restaurant/:id', function (req, res) {
+        // console.log(req.body);
+        var restaurantId = parseInt(req.params.id);
+        req.body.id = restaurantId;
+        var errorMessage = daoService.saveRestaurant(req.body);
+        var resVar = null;
+        if (errorMessage == null)
+        {
+            resVar = daoService.createIdResponse(req.body.id);
+        }
+        else
+        {
+            resVar = daoService.createError('Not Found', "NotFoundClass");
+            res.status(404);
+        }
+        res.json(resVar);
+
+    });
+
+
+
+
+
 };
 
 
