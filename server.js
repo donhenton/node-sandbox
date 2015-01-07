041f4962c3ea498e5e005f888aca60f43fb461e4
d <dhenton@localhost.com>
2015-01-07 15:40:31 -0600

    socket additions

--- server.js
+++ server.js
@@ -9,9 +9,21 @@ var express = require('./config/express');
 
 // Create a new Express application instance
 var app = express();
+var server = require('http').Server(app);
+//var io = require('socket.io')(server);
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
 
 // Use the Express application instance to listen to the '3000' port
-app.listen(3000);
+//app.listen(3000);
+server.listen(3000);
 
 // Log the server status to the console
 console.log('Server running at http://localhost:3000/');
