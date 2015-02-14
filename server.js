c6ff3d2b23c30d0046dd9c514ffdf2bd9ad98e45
d <dhenton@localhost.com>
2015-02-14 03:20:37 -0600

    ajusting for heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // Set the 'NODE_ENV' variable
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+
 // Load the 'express' module
 var express = require('./config/express');
 
@@ -11,10 +12,15 @@ var express = require('./config/express');
 var app = express();
 
 // Use the Express application instance to listen to the '3000' port
-app.listen(3000);
 
+
+var portVar = 3000;
+if (process.env.NODE_ENV === 'production') {
+    portVar = process.env.PORT;
+}  
+app.listen(portVar);
 // Log the server status to the console
-console.log('Server running at http://localhost:3000/');
+console.log('Server running at http://localhost:' + portVar+" ("+process.env.NODE_ENV+")");
 
 // Use the module.exports property to expose our Express application instance for external usage
 module.exports = app;
