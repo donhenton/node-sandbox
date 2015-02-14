dedebeef346b4a1029c1d3336622e7b9ded0563e
d <dhenton@localhost.com>
2015-02-14 04:32:53 -0600

    ajusting for heroku

--- config/env/production.js
+++ config/env/production.js
@@ -4,5 +4,6 @@
 // Set the 'production' environment configuration object
 module.exports = {
 	sessionSecret: 'productionSessionSecret',
-        db: {url: "mongodb://mongouser:mongo9000@ds029811.mongolab.com:29811/restaurant_collection"}
+       // db: {url: "mongodb://mongouser:mongo9000@ds029811.mongolab.com:29811/restaurant_collection"}
+       db: {url: process.env.MONGO_URI}
 };
