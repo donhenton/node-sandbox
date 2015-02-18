6d61ab3395edc608728409a9a23764ac689b972e
d <dhenton@localhost.com>
2015-02-18 08:24:45 -0600

    cookie demo

--- app/routes/cookies.server.routes.js
+++ app/routes/cookies.server.routes.js
@@ -5,16 +5,45 @@
 module.exports = function (app) {
 
 
-    
+    var processForgetMe =  function (req, res)
+    {
+        res.clearCookie('remember');
+        res.redirect('back');
+    }
+
+    var processRememberMe = function (req, res)
+    {
+        var minute = 60 * 1000;
+        //console.log("remember switch "+req.body.remember);
+        if (req.body.remember)
+            res.cookie('remember', 1, {maxAge: minute});
+        //this is a special directive to return to the referrer
+        res.redirect('back');
+    }
 
 
+    var cookiePageRender = function (req, res) {
 
-   
-    var cookiePageRender =function (req, res) {
+        //console.log("hitting cookie render");
+        var checked = "";
+        var rememberText = "Who are you?";
+        var action = "cookies/rememberMe";
+        var buttonText = "Submit"
         
-         
+        if (req.cookies.remember)
+        {
+            checked = "checked";
+            rememberText = "I do remember you.";
+            action = "cookies/forgetMe";
+            buttonText = "Forget Me";
+        }
+        
         res.render('cookies/cookies', {
-            title: 'Cookies' 
+            title: 'Cookies',
+            checked: checked,
+            rememberText: rememberText,
+            action: action,
+            buttonText: buttonText
         });
 
     };
@@ -24,5 +53,7 @@ module.exports = function (app) {
     // routes
     ///////////////////////////////////////////////////////////////////////
     app.get('/cookies.doc', cookiePageRender);
+    app.post('/cookies/rememberMe', processRememberMe);
+    app.post('/cookies/forgetMe', processForgetMe);
 
 };
