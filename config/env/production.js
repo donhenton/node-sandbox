687113898818fdd27efc76103e631b1dae0b06c6
d <dhenton@localhost.com>
2015-11-06 10:24:43 -0600

    setting production heroku vars

--- config/env/production.js
+++ config/env/production.js
@@ -6,5 +6,5 @@ module.exports = {
 	sessionSecret: 'productionSessionSecret',
        // db: {url: "mongodb://mongouser:#######@ds029811.mongolab.com:29811/restaurant_collection"}
        db: {url: process.env.MONGO_URI},
-       morgueDb: {url: process.env.MONGO_MORGE_URI}
+       morgueDb: {url: process.env.MONGO_MORGUE_URI}
 };
