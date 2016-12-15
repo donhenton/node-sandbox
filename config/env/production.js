2b1d620fd70b6a25c3f4495bfe3ef631f322ef9f
Don Henton <donhenton@gmail.com>
2016-12-15 09:12:33 -0600

    working on mailing

--- config/env/production.js
+++ config/env/production.js
@@ -2,9 +2,16 @@
 'use strict';
 
 // Set the 'production' environment configuration object
+
+// note development.js is no longer in the source tree to protect the innocent
+// it can be reproduced from this file
+
+
+
 module.exports = {
 	sessionSecret: 'productionSessionSecret',
        // db: {url: "mongodb://mongouser:#######@ds029811.mongolab.com:29811/restaurant_collection"}
        db: {url: process.env.MONGO_URI},
-       morgueDb: {url: process.env.MONGO_MORGUE_URI}
+       morgueDb: {url: process.env.MONGO_MORGUE_URI},
+       mailPassword: process.env.MAIL_PASSWORD
 };
