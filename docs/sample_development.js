d5dc8f7d8028d205f4e490fc49b9c9a84645e792
Don Henton <donhenton@gmail.com>
2016-12-17 10:51:39 -0600

    adding sample development js entry

---
+++ docs/sample_development.js
@@ -0,0 +1,7 @@
+module.exports = {
+	sessionSecret: 'developmentSessionSecret',
+       // db: {url: 'mongodb://user:password@server/restaurant_collection'}
+         db: {url: 'mongodb://localhost/restaurant_collection'},
+         morgueDb: {url: 'mongodb://localhost/morguefile'},
+         mailPassword: "samplePassword for expcalendar1000@gmail.com"
+};
